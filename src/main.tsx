c0ea7339ff4465d6294e0124786d0c2a98b1914f
Raad <rahdpasha.com@gmail.com>
2026-08-14 11:18:33 +0300

    Register PWA service worker

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,20 @@ import './index.css'
 import App from './App.tsx'
 import { I18nProvider } from './i18n'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(
+  document.getElementById('root')!
+).render(
   <StrictMode>
     <I18nProvider>
       <App />
     </I18nProvider>
   </StrictMode>,
 )
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    void navigator.serviceWorker.register(
+      '/sw.js'
+    )
+  })
+}
